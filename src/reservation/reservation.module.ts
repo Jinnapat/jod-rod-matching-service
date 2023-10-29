2add396401a5444f54fdd5f3e3d5b656a0dd7105
Jinnapat Yana <jaja_yana@hotmail.com>
2023-10-29 19:23:13 +0700

    remove ParkingSpaceModule

--- src/reservation/reservation.module.ts
+++ src/reservation/reservation.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ParkingSpaceModule } from 'src/parking-space/parking-space.module';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 
 @Module({
-  imports: [ParkingSpaceModule],
   providers: [ReservationService],
   controllers: [ReservationController],
 })
