08130a4efd15aee8b19f02c7193cc0a659f8a995
Jinnapat Yana <jaja_yana@hotmail.com>
2023-10-29 19:21:57 +0700

    remove parking space mod

--- src/parking-space/parking-space.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ParkingSpaceService } from './parking-space.service';
-import { ParkingSpaceController } from './parking-space.controller';
-
-@Module({
-  providers: [ParkingSpaceService],
-  controllers: [ParkingSpaceController],
-})
-export class ParkingSpaceModule {}
