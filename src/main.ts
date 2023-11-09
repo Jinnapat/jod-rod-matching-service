1d01bb5a6c128b24ab515fb92ade41157e746f78
Palm394 <palm.nichapat@gmail.com>
2023-11-09 15:48:39 +0700

    add service discovery

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { registerWithEureka } from './helper/eureka-helper'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  registerWithEureka("RESERVATION-SERVICE", 9000)
   await app.listen(3000);
 }
 bootstrap();
