4adf309d6da2f042cf8d5c61c576ca9199b21dfc
Jinnapat Yana <jaja_yana@hotmail.com>
2023-11-14 17:25:19 +0700

    Add ScheduleModule to root

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { ReservationController } from './reservation/reservation.controller';
 import { ReservationService } from './reservation/reservation.service';
 import { ReservationModule } from './reservation/reservation.module';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ReservationModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ReservationModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+  ],
   controllers: [AppController, ReservationController],
   providers: [AppService, ReservationService],
 })
